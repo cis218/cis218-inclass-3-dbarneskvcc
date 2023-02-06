b7e8cf15df596b8adeb47eeee91020d43967466f
David Barnes <dbarnes@kvcc.edu>
2023-02-06 18:32:31 -0500

    Add Post model. Register model with admin.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+from django.urls import reverse
+
+
+class Post(models.Model):
+    """Post model"""
+
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+    )
+    body = models.TextField()
+
+    def __str__(self):
+        """String method"""
+        return self.title
+
+    def get_absolute_url(self):
+        """Get absolute URL of object"""
+        return reverse(
+            "post_detail",
+            kwargs={"pk": self.pk},
+        )
