4f29804ffe7b6491ebcabdc5e5bd7e143698a9d2
David Barnes <dbarnes@kvcc.edu>
2023-02-20 16:44:18 -0500

    Add Gunicorn. Add Procfile and runtime.txt. Ready for heroku.

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-k=wn8397597vz%gi67&)dof53=pprl43&nv(jv(+-@6oe1!p-o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
